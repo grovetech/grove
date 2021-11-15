d562a5bcbfdcf8e1808ad40a92186b49eedc10c5
System Administrator <root@Jons-Mac.local>
2021-11-14 22:17:13 -0500

    Blog Posts

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,10 @@
 
 sudo bundle exec jekyll build
 
-sudo firebase deploy --only hosting:munki-182818
+#sudo firebase deploy --only hosting:munki-182818
+
+git add .
+
+git commit -m "Blog Posts"
+
+git push origin main
